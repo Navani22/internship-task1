60d77691d630a64a9eada1c610b5e7ffb0e70da8
Navani22 <swaminavani@gmail.com>
2024-01-18 13:13:01 +0530

    Adding code to practice files

---
+++ Practice1.java
@@ -0,0 +1,13 @@
+package internship-task1;
+
+public class Practice1 {
+    
+        public static void main(String[] args){
+     int a=5;
+     int b=7;
+     int c=15;
+     int sum=a+b+c;
+     System.out.println(sum);
+   }
+   }
+
--- task1.java
+++ task1.java
@@ -5,26 +5,28 @@ public class task1{
 
   public static void main(String[] args) {
 
-    System.out.println("Hello world!");
-    Scanner sc= new Scanner(System.in);
-    System.out.println("Enter Student Name :");
-    String studName=sc.next();
-    System.out.println("Enter Student Marks for Subject 1 :");
-    int studMark1=sc.nextInt();
-    System.out.println("Enter Student Marks for Subject 2 :");
-    int studMark2=sc.nextInt();
-    System.out.println("Enter Student Marks for Subject 3 :");
-    int stuMark3=sc.nextInt();
-    System.out.println("Enter Student Marks for Subject 4 :");
-    int studMark4=sc.nextInt();
-    System.out.println("Enter Student Marks for Subject 5 :");
-    int studMark5=sc.nextInt();
-    int totalMarks=studMark1+studMark2+stuMark3+studMark4+studMark5;
-    int percentage=totalMarks/5;
-    System.out.println("Precentage obtained by "+studName+" is "+ percentage+"%");
-    sc.close();
-
-  }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Student Name :");
+        String studName = sc.next();
+        System.out.println("Enter Student Marks for Subject 1 :");
+        float studMark1 = sc.nextFloat();
+        System.out.println("Enter Student Marks for Subject 2 :");
+        float studMark2 = sc.nextFloat();
+        System.out.println("Enter Student Marks for Subject 3 :");
+        float stuMark3 = sc.nextFloat();
+        System.out.println("Enter Student Marks for Subject 4 :");
+        float studMark4 = sc.nextFloat();
+        System.out.println("Enter Student Marks for Subject 5 :");
+        float studMark5 = sc.nextFloat();
+        float totalMarks = studMark1 + studMark2 + stuMark3 + studMark4 + studMark5;
+        float percentage = totalMarks / 5;
+        System.out.println("Precentage obtained by " + studName + " is " + percentage + "%");
+        sc.close();
+    
+      }
+    
+    }
 
   
-}
+
+  
